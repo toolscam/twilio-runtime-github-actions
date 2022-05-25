55bf35d08d00bb367fc5234d79d8c58fcf69f1d4
toolcam <presenter.simon@gmail.com>
2022-05-25 08:54:00 -0700

    mit

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,6 @@
 exports.handler = function(context, event, callback) {
     const result = {
-        message : "Hello World!"
+        message : "Hello Simon!"
     }
 
     callback(null, result);
